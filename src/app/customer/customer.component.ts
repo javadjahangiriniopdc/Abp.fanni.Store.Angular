bb6240c1e68cc71e009a0f212ef58b3fdff7efed
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-19 19:04:24 +0430

    NewCustomer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,23 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { CustomerService, CustomerDto } from '@proxy/customers';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.scss'],
-  providers: [ListService],
+  providers: [ListService,
+    { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }],
 })
 export class CustomerComponent implements OnInit {
   customer = { items: [], totalCount: 0 } as PagedResultDto<CustomerDto>;
 
-  constructor(public readonly list: ListService, private customerService: CustomerService) { }
+  form: FormGroup; // add this line
+
+  constructor(public readonly list: ListService,
+    private customerService: CustomerService,
+    private fb: FormBuilder // inject FormBuilder
+  ) { }
+  isModalOpen = false; // add this line
 
   ngOnInit(): void {
     const customerStreamCreator = (query) => this.customerService.getList(query);
 
     this.list.hookToQuery(customerStreamCreator).subscribe((response) => {
       this.customer = response;
+    });
+
+
+  }
+
+  // add new method
+  createBook() {
+    this.buildForm(); // add this line
+    this.isModalOpen = true;
+  }
+  // add buildForm method
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      family: [null, Validators.required],
+      brithday: [null, Validators.required],
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.customerService.create(this.form.value).subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
     });
   }
 
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { SharedModule } from '../shared/shared.module';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap'; // add this line
+
 
 
 @NgModule({
   declarations: [CustomerComponent],
   imports: [
     SharedModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    NgbDatepickerModule, // add this l
   ]
 })
 export class CustomerModule { }
