52b9fe9210d276aa99a62949f06812c29521d5c4
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-19 17:52:03 +0430

    ProductComponent

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { ProductService, ProductDto } from '@proxy/products';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
+  providers: [ListService],
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  product = { items: [], totalCount: 0 } as PagedResultDto<ProductDto>;
+  constructor(public readonly list: ListService, private productService: ProductService) { }
 
   ngOnInit(): void {
+    const productStreamCreator = (query) => this.productService.getList(query);
+
+    this.list.hookToQuery(productStreamCreator).subscribe((response) => {
+      this.product = response;
+    });
   }
 
 }
