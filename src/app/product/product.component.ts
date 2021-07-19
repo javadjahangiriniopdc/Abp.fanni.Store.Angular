8e542ecc6164b9c4a68e05f518fd4c6fd18a5994
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-19 20:32:09 +0430

    update and delete product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { ProductService, ProductDto, productTypeOptions } from '@proxy/products';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-product',
@@ -11,13 +12,16 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ProductComponent implements OnInit {
   product = { items: [], totalCount: 0 } as PagedResultDto<ProductDto>;
+  selectedProduct = {} as ProductDto; // declare selectedBook
   form: FormGroup; // add this line
   // add bookTypes as a list of BookType enum members
   productTypes = productTypeOptions;
   isModalOpen = false; // add this line
   constructor(public readonly list: ListService,
     private productService: ProductService,
-    private fb: FormBuilder // inject FormBuilde
+    private fb: FormBuilder, // inject FormBuilde
+    private confirmation: ConfirmationService // inject the ConfirmationService
+
   ) { }
 
   ngOnInit(): void {
@@ -30,29 +34,51 @@ export class ProductComponent implements OnInit {
 
   // add new method
   createProduct() {
+    this.selectedProduct = {} as ProductDto; // reset the selected book
     this.buildForm(); // add this line
     this.isModalOpen = true;
+  }
+  // Add editBook method
+  editBook(id: number) {
+    this.productService.get(id).subscribe((book) => {
+      this.selectedProduct = book;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
   }
   // add buildForm method
   buildForm() {
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      productType: [null, Validators.required],
-      pirce: [null, Validators.required],
+      name: [this.selectedProduct.name || '', Validators.required],
+      productType: [this.selectedProduct.productType || null, Validators.required],
+      pirce: [this.selectedProduct.pirce || null, Validators.required],
     });
   }
 
-  // add save method
+  // change the save method
   save() {
     if (this.form.invalid) {
       return;
     }
 
-    this.productService.create(this.form.value).subscribe(() => {
+    const request = this.selectedProduct.id
+      ? this.productService.update(this.selectedProduct.id, this.form.value)
+      : this.productService.create(this.form.value);
+
+    request.subscribe(() => {
       this.isModalOpen = false;
       this.form.reset();
       this.list.get();
     });
   }
+
+  // Add a delete method
+delete(id: number) {
+  this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe((status) => {
+    if (status === Confirmation.Status.confirm) {
+      this.productService.delete(id).subscribe(() => this.list.get());
+    }
+  });
+}
 
 }
