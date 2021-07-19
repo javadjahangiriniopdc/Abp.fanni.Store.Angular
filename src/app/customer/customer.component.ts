06aeee24ae481b4e114012075e4e3143c7803a32
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-19 17:36:21 +0430

    BookComponent

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { CustomerService, CustomerDto } from '@proxy/customers';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+  styleUrls: ['./customer.component.scss'],
+  providers: [ListService],
 })
 export class CustomerComponent implements OnInit {
+  customer = { items: [], totalCount: 0 } as PagedResultDto<CustomerDto>;
 
-  constructor() { }
+  constructor(public readonly list: ListService, private customerService: CustomerService) { }
 
   ngOnInit(): void {
+    const customerStreamCreator = (query) => this.customerService.getList(query);
+
+    this.list.hookToQuery(customerStreamCreator).subscribe((response) => {
+      this.customer = response;
+    });
   }
 
 }
