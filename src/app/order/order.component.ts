e0b1f81dcdbe3dfd8090d7fd1c844b51403446e7
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-19 18:08:32 +0430

    OrderComponent and fic title book and product

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { OrderService, OrderDto } from '@proxy/orders';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.scss']
+  styleUrls: ['./order.component.scss'],
+  providers: [ListService],
 })
 export class OrderComponent implements OnInit {
-
-  constructor() { }
+  order = { items: [], totalCount: 0 } as PagedResultDto<OrderDto>;
+  constructor(public readonly list: ListService, private orderService: OrderService) { }
 
   ngOnInit(): void {
+    const orderStreamCreator = (query) => this.orderService.getList(query);
+
+    this.list.hookToQuery(orderStreamCreator).subscribe((response) => {
+      this.order = response;
+    });
   }
 
 }
