f6b40263cce04a19eb264264ff15ca72d430e86f
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-07-19 19:59:02 +0430

    create product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService, PagedResultDto } from '@abp/ng.core';
-import { ProductService, ProductDto } from '@proxy/products';
+import { ProductService, ProductDto, productTypeOptions } from '@proxy/products';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
@@ -10,13 +11,47 @@ import { ProductService, ProductDto } from '@proxy/products';
 })
 export class ProductComponent implements OnInit {
   product = { items: [], totalCount: 0 } as PagedResultDto<ProductDto>;
-  constructor(public readonly list: ListService, private productService: ProductService) { }
+  form: FormGroup; // add this line
+  // add bookTypes as a list of BookType enum members
+  productTypes = productTypeOptions;
+  isModalOpen = false; // add this line
+  constructor(public readonly list: ListService,
+    private productService: ProductService,
+    private fb: FormBuilder // inject FormBuilde
+  ) { }
 
   ngOnInit(): void {
     const productStreamCreator = (query) => this.productService.getList(query);
 
     this.list.hookToQuery(productStreamCreator).subscribe((response) => {
       this.product = response;
+    });
+  }
+
+  // add new method
+  createProduct() {
+    this.buildForm(); // add this line
+    this.isModalOpen = true;
+  }
+  // add buildForm method
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      productType: [null, Validators.required],
+      pirce: [null, Validators.required],
+    });
+  }
+
+  // add save method
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.productService.create(this.form.value).subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
     });
   }
 
